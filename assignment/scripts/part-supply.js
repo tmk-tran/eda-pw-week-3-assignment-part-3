dc0cbcfd6727c560a7bfa606eb80f2f96441c251
T Mark <tmk1.tran@gmail.com>
2023-07-31 16:30:20 -0500

    started part 3

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -5,12 +5,21 @@ console.log('****** Part Supply *******');
 //    & console.log the variable
 console.log('1. Number of partsNeeded:');
 
+let partsNeeded = 40;
+console.log( partsNeeded );
+
+
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
 
+let supplyChanges = [ 3, 5, -6, 0, 7, 11 ];
+console.log( supplyChanges );
+
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
+
+
 
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
